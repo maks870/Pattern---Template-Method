11833e472e7477e2cf77c70a8ec7ba25ca4df27f
maks870 <maks7-2014@inbox.ru>
2023-06-24 18:07:02 +0300

    add royal adventure in Program.cs

--- Pattern - Template Method/Program.cs
+++ Pattern - Template Method/Program.cs
@@ -6,12 +6,16 @@ namespace Pattern___Template_Method
     {
         static void Main(string[] args)
         {
-            Hero hero = new Hero(3, 5);
+            Hero hero = new Hero(4, 6);
             Adventure adventure = new ForestAdventure(hero);
             adventure.GoToAdventure();
             Console.WriteLine();
 
             adventure = new DungeonAdventure(hero);
+            adventure.GoToAdventure();
+            Console.WriteLine();
+
+            adventure = new RoyalAdventure(hero);
             adventure.GoToAdventure();
 
             Console.ReadKey();
